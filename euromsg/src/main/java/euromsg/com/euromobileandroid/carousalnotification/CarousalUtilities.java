f20cb2770812024c04efe88d78ab835cbea99f8b
melikeyildirim <melike.yildirim@euromsg.com>
2020-02-17 16:27:34 +0300

    carousal implementation

---
+++ euromsg/src/main/java/euromsg/com/euromobileandroid/carousalnotification/CarousalUtilities.java
@@ -0,0 +1,149 @@
+package euromsg.com.euromobileandroid.carousalnotification;
+
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+
+public class CarousalUtilities {
+    public static Bitmap carousalDrawableToBitmap(Drawable drawable) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if (bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if (drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
+    }
+
+    /**
+     * retrieves app icon resource id
+     * **/
+    public static int carousalGetAppIconResourceId(Context context) {
+        int appIconResId = -1;
+        String packageName = context.getPackageName();
+        final PackageManager pm = context.getPackageManager();
+        final ApplicationInfo applicationInfo;
+        try {
+            applicationInfo = pm.getApplicationInfo(packageName, PackageManager.GET_META_DATA);
+            appIconResId = applicationInfo.icon;
+        } catch (PackageManager.NameNotFoundException e) {
+            //do nothing here
+        }
+        return appIconResId;
+    }
+
+    public static String carousalGetApplicationName(Context context) {
+        ApplicationInfo applicationInfo = context.getApplicationInfo();
+        int stringId = applicationInfo.labelRes;
+        return stringId == 0 ? applicationInfo.nonLocalizedLabel.toString() : context.getString(stringId);
+    }
+
+    /**
+     * saves bitmap to internal storage
+     * @param context
+     * @param bitmapImage
+     * @param fileName
+     * @return
+     */
+    public static String carousalSaveBitmapToInternalStorage(Context context, Bitmap bitmapImage, String fileName) {
+        boolean fileSaved = false;
+        ContextWrapper cw = new ContextWrapper(context.getApplicationContext());
+        // path to /data/data/yourapp/app_data/imageDir
+        File directory = cw.getDir("imageDir", Context.MODE_PRIVATE);
+        // Create imageDir
+        File mypath = new File(directory, fileName + ".jpg");
+
+        FileOutputStream fos = null;
+        try {
+            fos = new FileOutputStream(mypath);
+            // Use the compress method on the BitMap object to write image to the OutputStream
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fileSaved = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        if (fileSaved)
+            return directory.getAbsolutePath();
+        else
+            return null;
+    }
+
+    /**
+     * Returns image from internal storage
+     * @param path
+     * @param fileName
+     * @return
+     */
+    public static Bitmap carousalLoadImageFromStorage(String path, String fileName) {
+        Bitmap b = null;
+
+        try {
+            File f = new File(path, fileName + ".jpg");
+            b = BitmapFactory.decodeStream(new FileInputStream(f));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return b;
+    }
+
+    /**
+     * returns inSampleSize of an image for scaling
+     * @param width : current width
+     * @param height : current height
+     * @param reqWidth : required width
+     * @param reqHeight : required height
+     * @return inSmplesize --> power of 2
+     */
+    public static int carousalCalculateInSampleSize(
+            final int width, final int height, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+}
