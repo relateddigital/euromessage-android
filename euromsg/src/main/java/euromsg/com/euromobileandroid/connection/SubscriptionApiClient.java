6aeabdd09c656596e9ae293ba09b9cca2c56e51a
emre.eken <emre.eken@euromsg.com>
2021-09-02 16:05:43 +0300

    Makes timeout 45 sec

--- euromsg/src/main/java/euromsg/com/euromobileandroid/connection/SubscriptionApiClient.java
+++ euromsg/src/main/java/euromsg/com/euromobileandroid/connection/SubscriptionApiClient.java
@@ -29,7 +29,7 @@ public class SubscriptionApiClient {
                 new OkHttpClient.Builder()
                         .addInterceptor(new RawResponseInterceptor())
                         .addInterceptor(interceptor).connectTimeout(5, TimeUnit.MINUTES)
-                .readTimeout(10, TimeUnit.SECONDS);
+                .readTimeout(45, TimeUnit.SECONDS);
 
         if (retrofit == null) {
             retrofit = new Retrofit.Builder()
