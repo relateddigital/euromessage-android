d556886816cd5155b8512f92a0b6670d25d36bba
emre.eken <emre.eken@euromsg.com>
2021-09-03 18:43:36 +0300

    Makes timeouts for the requests 30 sec

--- euromsg/src/main/java/euromsg/com/euromobileandroid/connection/RetentionApiClient.java
+++ euromsg/src/main/java/euromsg/com/euromobileandroid/connection/RetentionApiClient.java
@@ -28,9 +28,9 @@ public class RetentionApiClient {
 
             OkHttpClient okHttpClient = new OkHttpClient.Builder()
                     .addInterceptor(interceptor)
-                    .connectTimeout(45, TimeUnit.SECONDS)
-                    .readTimeout(45, TimeUnit.SECONDS)
-                    .writeTimeout(45, TimeUnit.SECONDS)
+                    .connectTimeout(30, TimeUnit.SECONDS)
+                    .readTimeout(30, TimeUnit.SECONDS)
+                    .writeTimeout(30, TimeUnit.SECONDS)
                     .build();
             retrofit = new Retrofit.Builder()
                     .baseUrl("https://pushr.euromsg.com/")
