99a6050849feb4010d385a1b1d074f4c87cf1246
Umut Can Alparslan <umut_alparslan@hotmail.com>
2025-08-15 13:54:22 +0300

    added huawei plugins

--- app/src/main/java/com/relateddigital/euromessage/MainApplication.java
+++ app/src/main/java/com/relateddigital/euromessage/MainApplication.java
@@ -5,6 +5,9 @@ import android.util.Log;
 import com.google.firebase.messaging.FirebaseMessaging;
 import euromsg.com.euromobileandroid.EuroMobileManager;
 import euromsg.com.euromobileandroid.enums.RDNotificationPriority;
+import com.huawei.agconnect.AGConnectOptionsBuilder;
+import com.huawei.hms.aaid.HmsInstanceId;
+import com.huawei.hms.common.ApiException;
 
 
 
@@ -36,13 +39,32 @@ public class MainApplication extends Application {
         euroMobileManager.setPushIntent("com.relateddigital.euromessage.MainActivity", getApplicationContext());
         euroMobileManager.setNotificationPriority(RDNotificationPriority.NORMAL, getApplicationContext());
 
+        if (EuroMobileManager.checkPlayService(getApplicationContext())) {
+            setExistingFirebaseTokenToEuroMessage();
+        } else {
+            setHuaweiTokenToEuromessage();
+        }
+    }
 
-
-
-        setExistingFirebaseTokenToEuroMessage();
-
-
-
+    private void setHuaweiTokenToEuromessage() {
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    String appId = new AGConnectOptionsBuilder().build(getApplicationContext()).getString("client/app_id");
+                    final String token = HmsInstanceId.getInstance(getApplicationContext()).getToken(appId, "HCM");
+                    if(TextUtils.isEmpty(token) || token == null) {
+                        Log.e("Huawei Token : ", "Empty token!!!");
+                        return;
+                    }
+                    euroMobileManager.subscribe(token, getApplicationContext());
+                    SP.saveString(getApplicationContext(), "HuaweiToken", token);
+                    Log.i("Huawei Token", "" + token);
+                } catch (ApiException e) {
+                    Log.e("Huawei Token", "Getting the token failed! " + e);
+                }
+            }
+        }.start();
     }
 
 
